ebc15affebdda32d4cdf48759271b7e3792d5bc9
Jhon <jhon.aguilar@bbva.com>
2023-08-01 14:39:54 -0500

    refactor: PERAR-24033 PERAR-24358 Refactoring Insurance Quotation Life 2

--- artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/pattern/impl/QuotationParameter.java
+++ artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/pattern/impl/QuotationParameter.java
@@ -1,13 +1,19 @@
 package com.bbva.rbvd.lib.r304.pattern.impl;
 
 import com.bbva.elara.configuration.manager.application.ApplicationConfigurationService;
+import com.bbva.pisd.dto.insurance.aso.CustomerListASO;
 import com.bbva.pisd.lib.r350.PISDR350;
+import com.bbva.rbvd.dto.lifeinsrc.dao.InsuranceProductModalityDAO;
+import com.bbva.rbvd.dto.lifeinsrc.dao.ProductInformationDAO;
 import com.bbva.rbvd.dto.lifeinsrc.quotation.EasyesQuotationDTO;
 import com.bbva.rbvd.lib.r303.RBVDR303;
 import com.bbva.rbvd.lib.r304.pattern.PreQuotation;
 import com.bbva.rbvd.lib.r304.transfer.PayloadConfig;
+import com.bbva.rbvd.lib.r304.transfer.PayloadProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class QuotationParameter implements PreQuotation {
 
@@ -30,9 +36,20 @@ public class QuotationParameter implements PreQuotation {
         LOGGER.info("***** QuotationParameter getConfig START *****");
         LOGGER.info("***** QuotationParameter getConfig - input : {} *****",input);
         PayloadConfig payloadConfig = new PayloadConfig();
+        //PayloadProperties properties = this.getProperties(input);
+        //ProductInformationDAO productInformation = this.getProduct(input.getProduct().getId());
+
+        //CustomerListASO customerResponse = this.getCustomer(input.getHolder().getId());
+
+        //List<InsuranceProductModalityDAO> insuranceProductModalityDAOList =
+               // this.getModalities(this.getModalitiesSelected(input), productInformation.getInsuranceProductId(), input.getSaleChannelId());
+
 
         LOGGER.info("***** QuotationParameter getConfig - END  payloadConfig: {} *****",payloadConfig);
         return payloadConfig;
+
     }
+
+
 
 }
