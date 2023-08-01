c21e12bd42503e7cdac7b945f6d55fe732078cb2
Jhon <jhon.aguilar@bbva.com>
2023-08-01 17:23:07 -0500

    refactor: PERAR-24033 PERAR-24358 Refactoring Insurance Quotation Life 5

--- artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/transfer/QuotationStore.java
+++ artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/transfer/QuotationStore.java
@@ -1,5 +1,34 @@
 package com.bbva.rbvd.lib.r304.transfer;
 
+import com.bbva.rbvd.lib.r304.service.dao.impl.InsQuotInsertBoDAO;
+
 public class QuotationStore {
 
+    private String creationUser;
+    private String userAudit;
+    private InsQuotInsertBoDAO insQuotInsertBoDAO;
+
+    public String getUserAudit() {
+        return userAudit;
+    }
+
+    public void setUserAudit(String userAudit) {
+        this.userAudit = userAudit;
+    }
+
+    public String getCreationUser() {
+        return creationUser;
+    }
+
+    public void setCreationUser(String creationUser) {
+        this.creationUser = creationUser;
+    }
+
+    @Override
+    public String toString() {
+        return "QuotationStore{" +
+                "creationUser='" + creationUser + '\'' +
+                ", userAudit='" + userAudit + '\'' +
+                '}';
+    }
 }
