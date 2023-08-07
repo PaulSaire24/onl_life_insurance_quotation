2a9f2dc2d53aafaa4e30b6880cbb7fb9bd643865
Fredy Cerron <fredy.cerron.contractor@bbva.com>
2023-08-07 12:41:14 -0500

    refactor:PERAR-24033 PERAR-24410 test RBVDR304ImplTest continue 1

--- artifact/libraries/RBVDR304IMPL/src/test/java/com/bbva/rbvd/lib/r304/impl/RBVDR304ImplTest.java
+++ artifact/libraries/RBVDR304IMPL/src/test/java/com/bbva/rbvd/lib/r304/impl/RBVDR304ImplTest.java
@@ -4,32 +4,23 @@ import com.bbva.elara.configuration.manager.application.ApplicationConfiguration
 import com.bbva.elara.domain.transaction.Context;
 import com.bbva.elara.domain.transaction.ThreadContext;
 
-import com.bbva.pisd.dto.insurance.mock.MockDTO;
 import com.bbva.pisd.lib.r350.PISDR350;
 import com.bbva.rbvd.dto.lifeinsrc.dao.quotation.EasyesQuotationDAO;
 import com.bbva.rbvd.dto.lifeinsrc.mock.MockData;
 import com.bbva.rbvd.dto.lifeinsrc.quotation.EasyesQuotationDTO;
 
 import com.bbva.rbvd.dto.lifeinsrc.rimac.quotation.EasyesQuotationBO;
-import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDErrors;
 
-import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDProperties;
 import com.bbva.rbvd.lib.r303.RBVDR303;
 
-import com.bbva.rbvd.lib.r304.service.dao.IInsuranceSimulationDAO;
-import com.bbva.rbvd.lib.r304.service.dao.impl.InsurancePolicyDAO;
-import com.bbva.rbvd.lib.r304.service.dao.impl.InsuranceSimulationDAO;
-import com.bbva.rbvd.lib.r304.transform.bean.QuotationBean;
+import com.bbva.rbvd.lib.r304.transfer.PayloadConfig;
+import com.bbva.rbvd.lib.r304.transfer.PayloadProperties;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.Mock;
 
 import java.math.BigDecimal;
 
-import static java.util.Collections.singletonMap;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -37,9 +28,6 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static com.bbva.rbvd.dto.lifeinsrc.utils.RBVDValidation.build;
-
-import static org.mockito.Matchers.anyMap;
 
 public class RBVDR304ImplTest {
 
@@ -47,9 +35,12 @@ public class RBVDR304ImplTest {
     private RBVDR303 rbvdr303;
     private PISDR350 pisdR350;
     private EasyesQuotationDTO input;
-    private EasyesQuotationDAO easyesQuotationDao;
+    private EasyesQuotationDAO myQuotation;
     private EasyesQuotationBO rimacResponse;
-
+    private PayloadProperties properties;
+    private PayloadConfig payloadConfig;
+    @Mock
+    private ApplicationConfigurationService applicationConfigurationService;
 
     @Before
     public void setUp() throws Exception {
@@ -57,14 +48,26 @@ public class RBVDR304ImplTest {
 
         input = MockData.getInstance().getEasyesInsuranceQuotationRequest();
 
-        easyesQuotationDao = mock(EasyesQuotationDAO.class);
-        when(easyesQuotationDao.getInsuranceSimulationId()).thenReturn(BigDecimal.ONE);
+        myQuotation = mock(EasyesQuotationDAO.class);
+//        applicationConfigurationService = mock(ApplicationConfigurationService.class);
+
+        when(myQuotation.getInsuranceSimulationId()).thenReturn(BigDecimal.ONE);
 
         pisdR350 = mock(PISDR350.class);
 
-        ApplicationConfigurationService applicationConfigurationService = mock(ApplicationConfigurationService.class);
+        when(applicationConfigurationService.getProperty(anyString())).thenReturn("M");
 
-        when(applicationConfigurationService.getProperty("MONTHLY")).thenReturn("M");
+        properties = new PayloadProperties();
+        properties.setFrequencyTypeId("M");
+        properties.setSelectedPlanId("02");
+        properties.setProductType("840");
+        properties.setPeriodId("MONTHLY");
+        payloadConfig = new PayloadConfig();
+        payloadConfig.setInput(input);
+        payloadConfig.setPayloadProperties(properties);
+        payloadConfig.setMyQuotation(myQuotation);
+        payloadConfig.setPolicyQuotaId("8523654");
+
 
         //daoService = mock(DAOService.class);
         //rbvdr304.setDaoService(daoService);
@@ -95,9 +98,8 @@ public class RBVDR304ImplTest {
         //when(simulationDAO.executeGetSimulationInformation(input.getExternalSimulationId())).
         //        thenThrow(build(RBVDErrors.INVALID_RIMAC_QUOTATION_ID));
         //assertEquals(RBVDErrors.INVALID_RIMAC_QUOTATION_ID.getAdviceCode(), this.rbvdr304.getAdvice().getCode());
-
-
         EasyesQuotationDTO validation = this.rbvdr304.executeBusinessLogicEasyesQutation(input);
+
         assertNotNull(validation);
 
     }
