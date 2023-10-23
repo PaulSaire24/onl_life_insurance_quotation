c47a70c0e1cff98607c300afd5563e1301d5445b
Alec <alec.taboada.contractor@bbva.com>
2023-10-23 17:23:55 -0500

    feat: PERAR-25112 PERAR-25119 Create Query for Update DATABASE change querys 3

--- artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/transform/map/QuotationParticipantMap.java
+++ artifact/libraries/RBVDR304IMPL/src/main/java/com/bbva/rbvd/lib/r304/transform/map/QuotationParticipantMap.java
@@ -71,53 +71,5 @@ public class QuotationParticipantMap {
         LOGGER.info("QuotationParticipantMap end");
         return arguments;
     }
-    public static Map<String, Object> createArgumentsForUpdateParticipant(CommonsLifeDAO quotationParticipant){
-
-
-        LOGGER.info("QuotationParticipantMap start - createArgumentsForUpdateParticipant");
-        Map<String, Object> arguments = new HashMap<>();
-       arguments.put(RBVDProperties.FIELD_INSURED_AMOUNT.getValue(),quotationParticipant.getInsuredAmount());
-        LOGGER.info("FIELD_INSURED_AMOUNT Update : {}",quotationParticipant.getInsuredAmount());
-        arguments.put(RBVDProperties.FIELD_CURRENCY_ID.getValue(),quotationParticipant.getCurrencyId());
-        LOGGER.info("FIELD_CURRENCY_ID Update: {}",quotationParticipant.getCurrencyId());
-        arguments.put(RBVDProperties.FIELD_PERIOD_TYPE.getValue(),quotationParticipant.getPeriodType());
-        LOGGER.info("FIELD_PERIOD_TYPE Update: {}",quotationParticipant.getPeriodType());
-        arguments.put(RBVDProperties.FIELD_PERIOD_NUMBER.getValue(),quotationParticipant.getPeriodNumber());
-        LOGGER.info("FIELD_PERIOD_NUMBER Update {}",quotationParticipant.getPeriodNumber());
-        arguments.put(RBVDProperties.FIELD_REFUND_PER.getValue(),quotationParticipant.getRefundPer());
-        LOGGER.info("FIELD_REFUND_PER Update:{}",quotationParticipant.getRefundPer());
-        arguments.put(RBVDProperties.FIELD_TOTAL_RETURN_AMOUNT.getValue(),quotationParticipant.getTotalReturnAmount());
-        LOGGER.info("FIELD_TOTAL_RETURN_AMOUNT Update {}",quotationParticipant.getTotalReturnAmount());
-        arguments.put(RBVDProperties.FIELD_INSURED_ID.getValue(),quotationParticipant.getInsuredId());
-        LOGGER.info("FIELD_INSURED_ID Update {}",quotationParticipant.getInsuredId());
-        arguments.put(RBVDProperties.FIELD_CUSTOMER_DOCUMENT_TYPE.getValue(),quotationParticipant.getCustomerDocumentType());
-        LOGGER.info("FIELD_CUSTOMER_DOCUMENT_TYPE Update {}",quotationParticipant.getCustomerDocumentType());
-        arguments.put(RBVDProperties.FIELD_IS_BBVA_CUSTOMER_TYPE.getValue(),quotationParticipant.getIsBbvaCustomerType());
-        LOGGER.info("FIELD_IS_BBVA_CUSTOMER_TYPE Update {}",quotationParticipant.getIsBbvaCustomerType());
-        arguments.put(RBVDProperties.FIELD_PERSONAL_ID.getValue(),quotationParticipant.getPersonalId());
-        LOGGER.info("FIELD_PERSONAL_ID Update {}",quotationParticipant.getPersonalId());
-        arguments.put(RBVDProperties.FIELD_PHONE_ID.getValue(),quotationParticipant.getPhoneId());
-        LOGGER.info("FIELD_PHONE_ID Update {}",quotationParticipant.getPhoneId());
-        arguments.put(RBVDProperties.FIELD_CUSTOMER_ENTRY_DATE.getValue(),quotationParticipant.getCustomerEntryDate());
-        LOGGER.info("FIELD_CUSTOMER_ENTRY_DATE {}",quotationParticipant.getCustomerEntryDate());
-        arguments.put(RBVDProperties.FIELD_PARTICIPANT_ROLE_ID.getValue(),quotationParticipant.getParticipantRoleId());
-        LOGGER.info("FIELD_PARTICIPANT_ROLE_ID");
-        arguments.put(RBVDProperties.FIELD_INSURED_CUSTOMER_NAME.getValue(),quotationParticipant.getInsuredCustomerName());
-        LOGGER.info("QuotationParticipantMap FIELD_INSURED_CUSTOMER_NAME - FIELD_INSURED_CUSTOMER_NAME");
-        arguments.put(RBVDProperties.FIELD_CLIENT_LAST_NAME.getValue(),quotationParticipant.getClientLastName());
-        LOGGER.info("QuotationParticipantMap FIELD_CLIENT_LAST_NAME - FIELD_CLIENT_LAST_NAME");
-        arguments.put(RBVDProperties.FIELD_USER_EMAIL_PERSONAL_DESC.getValue(),quotationParticipant.getUserEmailPersonalDesc());
-        LOGGER.info("QuotationParticipantMap FIELD_USER_EMAIL_PERSONAL_DESC - FIELD_USER_EMAIL_PERSONAL_DESC");
-        arguments.put(RBVDProperties.FIELD_CUSTOMER_BIRTH_DATE.getValue(),quotationParticipant.getCustomerBirthDate());
-        LOGGER.info("QuotationParticipantMap FIELD_CUSTOMER_BIRTH_DATE - FIELD_CUSTOMER_BIRTH_DATE");
-        arguments.put(RBVDProperties.FIELD_CREATION_USER_ID.getValue(),quotationParticipant.getCreationUser());
-        LOGGER.info("QuotationParticipantMap FIELD_CREATION_USER_ID - FIELD_CREATION_USER_ID");
-        arguments.put(RBVDProperties.FIELD_USER_AUDIT_ID.getValue(),quotationParticipant.getUserAudit());
-        LOGGER.info("QuotationParticipantMap FIELD_USER_AUDIT_ID - FIELD_USER_AUDIT_ID");
-        arguments.put(RBVDProperties.FIELD_GENDER_ID.getValue(),quotationParticipant.getGenderId());
-        LOGGER.info("QuotationParticipantMap start - createArgumentsForSaveParticipant");
-        LOGGER.info("QuotationParticipantMap end");
-        return arguments;
-    }
 
 }
